5572935dc8c5d2f7b581177d9350f08bbd5a1e0b
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2024-02-20 23:52:16 +0200

    bundle gem gumroad-api

---
+++ spec/gumroad/api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Gumroad::Api do
+  it "has a version number" do
+    expect(Gumroad::Api::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
