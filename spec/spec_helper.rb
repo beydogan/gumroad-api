3c927d234c945c53c4f61316a1ade3ce554433b6
Mehmet Beydogan <mehmet.beydogan@gmail.com>
2024-02-21 16:49:49 +0200

    Fix spec_helper for token config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,18 +5,20 @@ require "vcr"
 require 'webmock'
 require "httparty"
 
+GUMROAD_ACCESS_TOKEN = ENV["GUMROAD_ACCESS_TOKEN"] || '<ACCESS_TOKEN>'
+
 VCR.configure do |config|
   config.allow_http_connections_when_no_cassette = true
   config.cassette_library_dir = "spec/cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
 
-  config.filter_sensitive_data('<ACCESS_TOKEN>') { ENV["GUMROAD_ACCESS_TOKEN"] }
+  config.filter_sensitive_data('<ACCESS_TOKEN>') { GUMROAD_ACCESS_TOKEN }
 end
 
 RSpec.configure do |config|
 
-  Gumroad::Client.access_token = ENV['GUMROAD_ACCESS_TOKEN']
+  Gumroad::Client.access_token = GUMROAD_ACCESS_TOKEN
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
